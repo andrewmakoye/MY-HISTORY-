f59b84114dee9fc47c5948d68045f841909d7ab0
andrewmakoye <mrandrews944@gmail.com>
2024-07-26 13:43:53 -0700

    Add files via upload

---
+++ connected.php
@@ -0,0 +1,43 @@
+<?php
+$con=mysqli_connect("localhost","root","","lasted");
+if(isset($_POST['save'])){
+
+$us=$_POST['username'];
+$e=$_POST['email'];
+$ps=$_POST['password'];
+
+$ins="INSERT INTO tutu(username,email,password) VALUES('$us','$e','$ps')";
+$run=mysqli_query($con,$ins);
+if($run){
+<?php
+$con=mysqli_connect("localhost","root","","lasted");
+if(isset($_POST['save'])){
+
+$us=$_POST['username'];
+$e=$_POST['email'];
+$ps=$_POST['password'];
+
+$ins="INSERT INTO tutu(username,email,password) VALUES('$us','$e','$ps')";
+$run=mysqli_query($con,$ins);
+if($run){
+header('Location: index.html');
+}
+else
+{
+echo"not okay";
+}
+}
+mysqli_close($con);
+
+
+?>
+}
+else
+{
+echo"not okay";
+}
+}
+mysqli_close($con);
+
+
+?>
---
+++ reg.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>lastly</title>
+</head>
+<body>
+<form action="connected.php"method="POST">
+<div>
+<label for="usename">username</label>
+<input type="text"name="username"required>
+</div>
+<div>
+<label for="email">email</label>
+<input type="text"name="email"required>
+</div>
+<div>
+<label for="password">password</label>
+<input type="text"name="password"required>
+</div>
+<br>
+<input type="submit" name="save"value="watumie">
+
+
+</form>
+</body>
+</html>
